d09f015655466fe8edb8111d8c230724a7d25884
Mohammed Irtisam Sajin <irtisam.coding@gmail.com>
2024-06-09 17:18:04 +0600

    Update

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import express, { Express } from "express";
 dotenv.config();
 const app: Express = express();
 app.use(cors());
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log("Server is listening on port ", process.env.PORT);
